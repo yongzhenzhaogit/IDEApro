601bf9fb0d3a5ae2721e6ec4365a2b920d8f71fa
zyz <zhaoyongzhenzyz@163.com>
2019-09-21 00:35:44 +0800

    day04_git

---
+++ day4-code/src/SannerDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SannerDemo {
+    public static void main(String[] args) {
+        Scanner sanKey = new Scanner(System.in);
+        int a = sanKey.nextInt();
+        int b= sanKey.nextInt();
+
+
+        System.out.println("a+b = "+ a+b);
+        System.out.println(sanKey.nextInt()+"yyy");
+    }
+}
---
+++ day4-code/src/cn/hello/demo1/HelloWorld.java
@@ -0,0 +1,15 @@
+package cn.hello.demo1;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int[] arry = {1,2,2,3,5,7};
+
+        System.out.println(Arrays.toString(arry));
+    }
+
+
+
+}
